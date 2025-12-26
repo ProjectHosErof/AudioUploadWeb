bd99072eaf0f554af769f04412dc4c7112e50165
anthonyriad <131625149+anthriad@users.noreply.github.com>
2025-12-25 18:59:05 -0800

    terraform initial commit

---
+++ phe-landing-page/cloudflare.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
